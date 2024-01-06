883d6921c031d5439ab80ee8f1f03944cd70e615
Yury Trembach <yury.trembach@gmail.com>
2024-01-06 23:42:35 +0200

    LogSources enum added
    SYSTEM
    CREATURE

---
+++ life/src/main/java/org/yt/jr/projects/utils/logs/LogSources.java
@@ -0,0 +1,16 @@
+package org.yt.jr.projects.utils.logs;
+
+public enum LogSources {
+    SYSTEM("system"),
+    CREATURE("creature");
+
+    private String message;
+
+    LogSources(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
