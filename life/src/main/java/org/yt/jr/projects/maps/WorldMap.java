fae1e51a70d331b15f8797d5934979b56b845a8a
Yury Trembach <yury.trembach@gmail.com>
2024-01-06 23:54:19 +0200

    Class WorldMap -the game field
    Init localions

---
+++ life/src/main/java/org/yt/jr/projects/maps/WorldMap.java
@@ -0,0 +1,70 @@
+package org.yt.jr.projects.maps;
+
+import org.yt.jr.projects.utils.Config;
+import org.yt.jr.projects.utils.logs.LogLevels;
+import org.yt.jr.projects.utils.logs.LogSources;
+import org.yt.jr.projects.utils.logs.Logger;
+
+public class WorldMap {
+    int width;
+    int height;
+    private Location[][] grid;
+
+    private static WorldMap WorldMAP;
+
+    public static WorldMap getMap() {
+        if (WorldMAP == null) {
+            WorldMAP = new WorldMap();
+            WorldMAP.mapInit();
+            Logger.Log(LogSources.SYSTEM, LogLevels.INFO, String.format("world map %dx%d created",
+                    WorldMAP.width, WorldMAP.height));
+        }
+        return WorldMAP;
+    }
+
+    private WorldMap() {
+        this.width = Config.CONFIG.getMapWidth();
+        this.height = Config.CONFIG.getMapHeight();
+        this.grid = new Location[width][height];
+    }
+
+    private void mapInit() {
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                Location newLocation = new Location(row, col);
+                grid[row][col] = newLocation;
+                setNeighbors(row, col);
+            }
+        }
+    }
+
+    private void setNeighbors(int row, int col) {
+        Location cell = grid[row][col];
+
+        Location up = row > 0 ? grid[row - 1][col] : null;
+        cell.setUp(up);
+        if (up != null) {
+            up.setDown(cell);
+        }
+
+        Location down = row < height - 1 ? grid[row + 1][col] : null;
+        cell.setDown(down);
+        if (down != null) {
+            down.setUp(cell);
+        }
+
+        Location left = col > 0 ? grid[row][col - 1] : null;
+        cell.setLeft(left);
+        if (left != null) {
+            left.setRight(cell);
+        }
+
+        Location right = col < width - 1 ? grid[row][col + 1] : null;
+        cell.setRight(right);
+        if (right != null) {
+            right.setLeft(cell);
+        }
+    }
+
+
+}
