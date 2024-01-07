a54e518e5809ba72248a1fd5e5ed4a2eacfc48b6
Yury Trembach <yury.trembach@gmail.com>
2024-01-07 22:57:58 +0200

    add two animal types

--- life/src/main/java/org/yt/jr/projects/creatures/lifecycles/CreatureFactory.java
+++ life/src/main/java/org/yt/jr/projects/creatures/lifecycles/CreatureFactory.java
@@ -2,6 +2,8 @@ package org.yt.jr.projects.creatures.lifecycles;
 
 import org.yt.jr.projects.creatures.Creature;
 import org.yt.jr.projects.creatures.Plant;
+import org.yt.jr.projects.creatures.animals.Deer;
+import org.yt.jr.projects.creatures.animals.Horse;
 import org.yt.jr.projects.utils.CreaturesTypes;
 
 import java.util.Map;
@@ -11,7 +13,11 @@ public class CreatureFactory {
     final public static CreatureFactory CREATURE_FACTORY = new CreatureFactory();
 
     final private Map<CreaturesTypes, Function<Integer,Creature>> generators = Map.of(
-            CreaturesTypes.PLANTS, Plant::new);
+            CreaturesTypes.PLANTS, Plant::new,
+
+            CreaturesTypes.HORSE, Horse::new,
+            CreaturesTypes.DEER, Deer::new
+    );
 
     public Function<Integer,Creature> getCreature(CreaturesTypes type) {
         return generators.get(type);
