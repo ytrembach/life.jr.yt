4ae0e6bbbb806577e5d7fca9d72e5260af03ae8e
Yury Trembach <yury.trembach@gmail.com>
2024-01-07 21:11:39 +0200

    add life cycles types move synchronization to initLocation

--- life/src/main/java/org/yt/jr/projects/creatures/lifecycles/LifeCycleCreator.java
+++ life/src/main/java/org/yt/jr/projects/creatures/lifecycles/LifeCycleCreator.java
@@ -1,32 +1,40 @@
 package org.yt.jr.projects.creatures.lifecycles;
 
 import org.yt.jr.projects.creatures.Creature;
-import org.yt.jr.projects.creatures.CreatureList;
 import org.yt.jr.projects.maps.Location;
 import org.yt.jr.projects.maps.WorldMap;
 import org.yt.jr.projects.utils.Config;
 import org.yt.jr.projects.utils.CreaturesTypes;
 
 public class LifeCycleCreator {
-    public static LifeCycle createLifeCycle(final CreaturesTypes type) {
+    public static LifeCycle createLifeCycle(final LifeCycleTypes type) {
         final LifeCycle lifeCycle = new LifeCycle(type);
 
         for (Location location : WorldMap.getMap()) {
-            initLocation(lifeCycle, location, type);
+            for (CreaturesTypes creaturesType : lifeCycle.getType().getTypes()) {
+                initLocation(lifeCycle, location, creaturesType);
+                LifeCycle.LIFECYCLES.put(creaturesType, lifeCycle);
+            }
         }
-
-        LifeCycle.LIFECYCLES.put(type, lifeCycle);
         return lifeCycle;
     }
 
     private static void initLocation(final LifeCycle lifeCycle, final Location location, final CreaturesTypes type) {
-        final CreatureList habitants = location.getHabitants();
-        final int maxCreaturePerLocation = Config.CONFIG.getMaxCreaturePerLocation(type);
+        final int maxCreatures = Config.CONFIG.getMaxCreaturePerLocation(type);
+        final double initShare = Config.CONFIG.getInitCreaturePerLocationShare(type);
 
-        for (int c = 0; c < 5; c++) {
+        final long initCreatureCount = Math.round(maxCreatures * initShare);
+
+        for (int c = 0; c < initCreatureCount; c++) {
             final Creature creature = CreatureFactory.CREATURE_FACTORY.getCreature(type).apply(Creature.MAX_HEALTH);
-            location.addCreature(creature);
-            lifeCycle.addCreature(creature);
+            synchronized (creature) {
+                synchronized (location) {
+                    location.addCreature(creature);
+                }
+                synchronized (lifeCycle) {
+                    lifeCycle.addCreature(creature);
+                }
+            }
         }
     }
 
