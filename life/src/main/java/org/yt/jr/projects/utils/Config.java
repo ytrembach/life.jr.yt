969051f6605a1cac38f74aa8e8dd5a29606a14e2
Yury Trembach <yury.trembach@gmail.com>
2024-01-07 14:38:12 +0200

    added logLevel added default values for parameters

--- life/src/main/java/org/yt/jr/projects/utils/Config.java
+++ life/src/main/java/org/yt/jr/projects/utils/Config.java
@@ -1,4 +1,7 @@
 package org.yt.jr.projects.utils;
+
+import org.yt.jr.projects.utils.logs.LogLevels;
+
 import java.util.Map;
 
 public class Config {
@@ -9,35 +12,40 @@ public class Config {
         return "/home/yt/IdeaProjects/tmp/life.jr.log";
     }
 
+    public LogLevels getLogLevel() {
+        return  LogLevels.DEBUG;
+    }
+
     public int getPoolSize(CreaturesTypes type) {
-        final Map<CreaturesTypes, Integer> sizes = Map.of(
-                CreaturesTypes.PLANTS, 4
+        final Map<CreaturesTypes, Integer> poolSizes = Map.of(
+                CreaturesTypes.ALL, 4
         );
 
-        return sizes.get(type);
+        return poolSizes.getOrDefault(type, poolSizes.get(CreaturesTypes.ALL));
     }
 
     public long getTurnDurationInSecond(CreaturesTypes type) {
         final Map<CreaturesTypes, Long> durations = Map.of(
                 CreaturesTypes.ALL, 1L
         );
-        return durations.get(type);
+        return durations.getOrDefault(type, durations.get(CreaturesTypes.ALL) );
     }
 
     public int getMaxCreaturePerLocation(CreaturesTypes type) {
         final Map<CreaturesTypes, Integer> maxNumbers = Map.of(
+                CreaturesTypes.ALL, 10,
                 CreaturesTypes.PLANTS, 200
         );
 
-        return maxNumbers.get(type);
+        return maxNumbers.getOrDefault(type, maxNumbers.get(CreaturesTypes.ALL));
     }
 
     public Double getReproduceProbability(CreaturesTypes type) {
         final Map<CreaturesTypes, Double> probabilities = Map.of(
-                CreaturesTypes.PLANTS, 0.1
+                CreaturesTypes.ALL, 0.1
         );
 
-        return probabilities.get(type);
+        return probabilities.getOrDefault(type, probabilities.get(CreaturesTypes.ALL));
     }
 
     public int getMapWidth() {
