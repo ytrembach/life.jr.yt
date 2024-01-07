c941590c42555efb5fd1982cf1a513597aeb63a2
Yury Trembach <yury.trembach@gmail.com>
2024-01-07 02:08:36 +0200

    small changes

--- life/src/main/java/org/yt/jr/projects/creatures/Plant.java
+++ life/src/main/java/org/yt/jr/projects/creatures/Plant.java
@@ -2,11 +2,14 @@ package org.yt.jr.projects.creatures;
 
 import org.yt.jr.projects.utils.Config;
 import org.yt.jr.projects.utils.CreaturesTypes;
+import org.yt.jr.projects.utils.logs.LogLevels;
+import org.yt.jr.projects.utils.logs.LogSources;
+import org.yt.jr.projects.utils.logs.Logger;
 
 public class Plant extends Creature {
     @Override
     public void run() {
-
+        Logger.Log(LogSources.CREATURE, LogLevels.DEBUG,String.format("plant running"));
     }
 
     @Override
