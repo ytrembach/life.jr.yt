a3165de3731dd867e4d5b50b0c39714f0773dcf6
Yury Trembach <yury.trembach@gmail.com>
2024-01-07 22:56:23 +0200

    Add two new animal classes

---
+++ life/src/main/java/org/yt/jr/projects/creatures/animals/Deer.java
@@ -0,0 +1,21 @@
+package org.yt.jr.projects.creatures.animals;
+
+import org.yt.jr.projects.creatures.Animal;
+import org.yt.jr.projects.utils.CreaturesTypes;
+
+public class Deer extends Animal {
+
+    public Deer(int health) {
+        super(CreaturesTypes.DEER, health);
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
---
+++ life/src/main/java/org/yt/jr/projects/creatures/animals/Horse.java
@@ -0,0 +1,21 @@
+package org.yt.jr.projects.creatures.animals;
+
+import org.yt.jr.projects.creatures.Animal;
+import org.yt.jr.projects.utils.CreaturesTypes;
+
+public class Horse extends Animal {
+
+    public Horse(int health) {
+        super(CreaturesTypes.HORSE, health);
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
