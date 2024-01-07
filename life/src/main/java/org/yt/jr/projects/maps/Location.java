230427024af1d2a197211ebaf7e2f8a635de9cea
Yury Trembach <yury.trembach@gmail.com>
2024-01-07 14:29:54 +0200

    rewrite neighbors adding creature to location

--- life/src/main/java/org/yt/jr/projects/maps/Location.java
+++ life/src/main/java/org/yt/jr/projects/maps/Location.java
@@ -3,49 +3,61 @@ package org.yt.jr.projects.maps;
 import org.yt.jr.projects.creatures.Creature;
 import org.yt.jr.projects.creatures.CreatureList;
 import org.yt.jr.projects.utils.CreaturesTypes;
+import org.yt.jr.projects.utils.logs.LogLevels;
+import org.yt.jr.projects.utils.logs.LogSources;
+import org.yt.jr.projects.utils.logs.Logger;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Location {
     final private int row;
     final private int col;
     // neighboring locations
-    private Location up, down, left, right;
+    private Map<LocationNeighbors,Location> neighbors;
     final private CreatureList habitants;
 
-    final private HashMap<CreaturesTypes,Integer> creaturesCount;
+    final private Map<CreaturesTypes,Integer> creaturesCount;
 
     Location(int row, int col) {
         this.row = row;
         this.col = col;
         habitants = new CreatureList();
+        neighbors = new HashMap<>();
 
         creaturesCount = new HashMap<>();
-    }
-
-    void setUp(Location up) {
-        this.up = up;
-    }
-
-    void setDown(Location down) {
-        this.down = down;
-    }
-
-    void setLeft(Location left) {
-        this.left = left;
-    }
-
-    void setRight(Location right) {
-        this.right = right;
     }
 
     public CreatureList getHabitants() {
         return habitants;
     }
 
-    public void addCreature(Creature creature) {
-        synchronized (creature) {
+    public Integer getCreaturesCount(CreaturesTypes type) {
+        return creaturesCount.get(type);
+    }
 
+    @Override
+    public String toString() {
+        return "Location{" +
+                "row=" + row +
+                ", col=" + col +
+                ", creaturesCount=" + creaturesCount +
+                '}';
+    }
+
+    void setNeighbor(LocationNeighbors direction, Location neighbor) {
+        this.neighbors.put(direction,neighbor);
+    }
+
+    public void addCreature(Creature creature) {
+        CreaturesTypes type = creature.getType();
+        synchronized (creature) {
+            synchronized (this) {
+                habitants.add(creature);
+                creature.setLocation(this);
+                creaturesCount.put(type,creaturesCount.getOrDefault(type,0)+1);
+            }
         }
+        Logger.Log(LogSources.CREATURE, LogLevels.INFO,String.format("%s added to %s",creature, this));
     }
 }
---
+++ life/src/main/java/org/yt/jr/projects/maps/LocationNeighbors.java
@@ -0,0 +1,5 @@
+package org.yt.jr.projects.maps;
+
+public enum LocationNeighbors {
+    UP,DOWN,LEFT,RIGHT
+}
