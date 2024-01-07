429273be0ab688ceda411c461c86ae242046248f
Yury Trembach <yury.trembach@gmail.com>
2024-01-07 14:44:37 +0200

    added first initLocation code

--- life/src/main/java/org/yt/jr/projects/creatures/lifecycles/LifeCycleCreator.java
+++ life/src/main/java/org/yt/jr/projects/creatures/lifecycles/LifeCycleCreator.java
@@ -1,5 +1,6 @@
 package org.yt.jr.projects.creatures.lifecycles;
 
+import org.yt.jr.projects.creatures.Creature;
 import org.yt.jr.projects.creatures.CreatureList;
 import org.yt.jr.projects.maps.Location;
 import org.yt.jr.projects.maps.WorldMap;
@@ -12,18 +13,23 @@ public class LifeCycleCreator {
     public static LifeCycle createLifeCycle(final CreaturesTypes type) {
         final LifeCycle lifeCycle = new LifeCycle(type);
 
-        final Iterator<Location> mapIterator = WorldMap.getMap().iterator();
-        while (mapIterator.hasNext()) {
-            final Location location = mapIterator.next();
-            initLocation(lifeCycle,location, type);
+        for (Location location : WorldMap.getMap()) {
+            initLocation(lifeCycle, location, type);
         }
 
+        LifeCycle.LIFECYCLES.put(type, lifeCycle);
         return lifeCycle;
     }
 
     private static void initLocation(final LifeCycle lifeCycle, final Location location, final CreaturesTypes type) {
         final CreatureList habitants = location.getHabitants();
-        final double maxCreaturePerLocation = Config.CONFIG.getMaxCreaturePerLocation(type);
+        final int maxCreaturePerLocation = Config.CONFIG.getMaxCreaturePerLocation(type);
+
+        for (int c = 0; c < 5; c++) {
+            final Creature creature = CreatureFactory.CREATURE_FACTORY.getCreature(type).apply(Creature.MAX_HEALTH);
+            location.addCreature(creature);
+            lifeCycle.addCreature(creature);
+        }
     }
 
 }
