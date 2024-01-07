c4fd338877f7f2ea36d067db88a879535df13567
Yury Trembach <yury.trembach@gmail.com>
2024-01-07 14:40:59 +0200

    made AutoCloseable added logging created static list of existing lifecycles

--- life/src/main/java/org/yt/jr/projects/creatures/lifecycles/LifeCycle.java
+++ life/src/main/java/org/yt/jr/projects/creatures/lifecycles/LifeCycle.java
@@ -3,17 +3,23 @@ package org.yt.jr.projects.creatures.lifecycles;
 import org.yt.jr.projects.creatures.Creature;
 import org.yt.jr.projects.utils.Config;
 import org.yt.jr.projects.utils.CreaturesTypes;
+import org.yt.jr.projects.utils.logs.LogLevels;
+import org.yt.jr.projects.utils.logs.LogSources;
+import org.yt.jr.projects.utils.logs.Logger;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
-public class LifeCycle {
+public class LifeCycle implements AutoCloseable {
+
+    final public static Map<CreaturesTypes,LifeCycle> LIFECYCLES = new HashMap<>();
 
     final private CreaturesTypes type;
-    final private HashMap<Creature, ScheduledFuture<?>> liveCreatures;
+    final private Map<Creature, ScheduledFuture<?>> liveCreatures;
 
     final private ScheduledExecutorService executorService;
 
@@ -25,12 +31,20 @@ public class LifeCycle {
         executorService = Executors.newScheduledThreadPool(poolSize);
     }
 
+    @Override
+    public String toString() {
+        return "LifeCycle{" +
+                "type=" + type +
+                '}';
+    }
+
     public void addCreature(Creature creature) {
         long turnDuration = Config.CONFIG.getTurnDurationInSecond(type);
         ScheduledFuture<?> future = executorService.scheduleAtFixedRate(creature, turnDuration, turnDuration, TimeUnit.SECONDS);
         synchronized (liveCreatures) {
             liveCreatures.put(creature, future);
         }
+        Logger.Log(LogSources.LIFECYCLE, LogLevels.INFO,String.format("%s added to %s",creature,this));
     }
 
     public void removeCreature(Creature creature) {
@@ -39,5 +53,10 @@ public class LifeCycle {
         synchronized (liveCreatures) {
             liveCreatures.remove(creature);
         }
+    }
+
+    @Override
+    public void close() throws Exception {
+        executorService.shutdown();
     }
 }
