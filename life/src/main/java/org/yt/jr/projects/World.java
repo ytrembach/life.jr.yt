8bdea2c570b718028570707e79c3454929cddda1
Yury Trembach <yury.trembach@gmail.com>
2024-01-07 14:50:43 +0200

    rewrite to try-with-resources added watch loop

--- life/src/main/java/org/yt/jr/projects/World.java
+++ life/src/main/java/org/yt/jr/projects/World.java
@@ -5,7 +5,11 @@ import org.yt.jr.projects.creatures.lifecycles.LifeCycleCreator;
 import org.yt.jr.projects.maps.WorldMap;
 import org.yt.jr.projects.utils.Config;
 import org.yt.jr.projects.utils.CreaturesTypes;
+import org.yt.jr.projects.utils.logs.LogLevels;
+import org.yt.jr.projects.utils.logs.LogSources;
 import org.yt.jr.projects.utils.logs.Logger;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class World {
 
@@ -14,9 +18,43 @@ public class World {
 
         WorldMap worldMap = WorldMap.getMap();
 
-        LifeCycle plantsLifeCycle = LifeCycleCreator.createLifeCycle(CreaturesTypes.PLANTS);
+        try (LifeCycle plantsLifeCycle = LifeCycleCreator.createLifeCycle(CreaturesTypes.PLANTS)) {
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+            String userInput;
 
+            prompt();
+            userInput = console.readLine();
+            while (!"EXIT".equals(userInput.toUpperCase())) {
+                String[] items = userInput.split("\s+");
+                if (items.length == 2) {
+                    try {
+                        int row = Integer.parseInt(items[0]);
+                        int col = Integer.parseInt(items[1]);
+
+                        if (row > 0 && row < Config.CONFIG.getMapHeight()
+                                && col > 0 && col < Config.CONFIG.getMapWidth()) {
+                            System.out.println(worldMap.getLocation(row, col));
+                        } else {
+                            System.out.printf("Location with row %d and col %s is outside the map", row, col);
+                        }
+                    } catch (NumberFormatException e) {
+
+                    }
+                }
+                prompt();
+                userInput = console.readLine();
+            }
+        } catch (Exception e) {
+            Logger.Log(LogSources.SYSTEM, LogLevels.FATAL, e.getMessage());
+            for (StackTraceElement el : e.getStackTrace()) {
+                Logger.Log(LogSources.SYSTEM, LogLevels.FATAL, el.toString());
+            }
+        }
         Logger.closeLogger();
+
     }
 
+    public static void prompt() {
+        System.out.print("Enter row and col pair separated by space to check location: ");
+    }
 }
