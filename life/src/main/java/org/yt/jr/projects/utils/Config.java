2b66e4a09e0c1455a868f788d1c38e352f7a12ae
Yury Trembach <yury.trembach@gmail.com>
2024-01-12 01:46:57 +0200

    move max and child health parameters here from Creature
    rewrite getTurnDurationInSecond to getTurnsPerMove

--- life/src/main/java/org/yt/jr/projects/utils/Config.java
+++ life/src/main/java/org/yt/jr/projects/utils/Config.java
@@ -1,5 +1,6 @@
 package org.yt.jr.projects.utils;
 
+import org.yt.jr.projects.creatures.CreaturesTypes;
 import org.yt.jr.projects.creatures.lifecycles.LifeCycleTypes;
 import org.yt.jr.projects.utils.logs.LogLevels;
 
@@ -9,12 +10,15 @@ public class Config {
 
     final public static Config CONFIG = new Config();
 
+    final public static int CREATURE_MAX_HEALTH = 100;
+    final public static int CREATURE_CHILD_HEALTH = 10;
+
     public String getLogFilePath() {
         return "/home/yt/IdeaProjects/tmp/life.jr.log";
     }
 
     public LogLevels getLogLevel() {
-        return  LogLevels.DEBUG;
+        return LogLevels.DEBUG;
     }
 
     public int getPoolSize(LifeCycleTypes type) {
@@ -23,15 +27,6 @@ public class Config {
 
         );
         return poolSizes.getOrDefault(type, valueDefault);
-    }
-
-    public long getTurnDurationInSecond(LifeCycleTypes type) {
-        long valueDefault = 1;
-        final Map<LifeCycleTypes, Long> durations = Map.of(
-                LifeCycleTypes.PLANTS, 5L,
-                LifeCycleTypes.HERBIVORES, 1L
-        );
-        return durations.getOrDefault(type, valueDefault );
     }
 
     public int getMaxCreaturePerLocation(CreaturesTypes type) {
@@ -43,28 +38,48 @@ public class Config {
     }
 
     public Double getInitCreaturePerLocationShare(CreaturesTypes type) {
-        double valueDefault = 0.1;
+        double valueDefault = 0.5;
         final Map<CreaturesTypes, Double> shares = Map.of(
-                CreaturesTypes.PLANTS, 0.25
+                CreaturesTypes.PLANTS, 0.1
         );
         return shares.getOrDefault(type, valueDefault);
+    }
+
+    public int getTurnsPerMove(LifeCycleTypes type) {
+        int valueDefault = 1;
+        final Map<LifeCycleTypes, Integer> durations = Map.of(
+                LifeCycleTypes.PLANTS, 5,
+                LifeCycleTypes.HERBIVORES, 3,
+                LifeCycleTypes.CARNIVORES, 1
+        );
+        return durations.getOrDefault(type, valueDefault);
+    }
+
+    public int getTurnsToReproduceDefault(CreaturesTypes type) {
+        int valueDefault = 1;
+        final Map<CreaturesTypes, Integer> probabilities = Map.of(
+                CreaturesTypes.HORSE, 5,
+                CreaturesTypes.DEER, 5
+        );
+        return probabilities.getOrDefault(type, valueDefault);
     }
 
     public Double getReproduceProbability(CreaturesTypes type) {
         double valueDefault = 0.1;
         final Map<CreaturesTypes, Double> probabilities = Map.of(
-                CreaturesTypes.PLANTS, 0.05,
+                CreaturesTypes.PLANTS, 0.2,
                 CreaturesTypes.HORSE, 0.15,
-                CreaturesTypes.DEER, 0.1
+                CreaturesTypes.DEER, 0.10
         );
         return probabilities.getOrDefault(type, valueDefault);
     }
 
-    public int getTurnsToReproduceDefault(CreaturesTypes type) {
-        int valueDefault = 10;
+    public int getMaxAge(CreaturesTypes type) {
+        int valueDefault = 1;
         final Map<CreaturesTypes, Integer> probabilities = Map.of(
-                CreaturesTypes.HORSE, 2 ,
-                CreaturesTypes.DEER, 3
+                CreaturesTypes.PLANTS, 200,
+                CreaturesTypes.HORSE, 80,
+                CreaturesTypes.DEER, 100
         );
         return probabilities.getOrDefault(type, valueDefault);
     }
