c55770f6779ba462ddac9caf32c84430234a1241
Yury Trembach <yury.trembach@gmail.com>
2024-01-12 01:34:53 +0200

    add Location id
    add Atomic lastId - unique Location counter
    
    add removeCreature - delete creature from location
    
    add static empty Location NOWHERE and prviate default constructor for it. id=0, the rest locations begin from 1
    
    rewrite isSpaceAvailable to checkSpaceAvailable with NOWHERE processing and logging

--- life/src/main/java/org/yt/jr/projects/maps/Location.java
+++ life/src/main/java/org/yt/jr/projects/maps/Location.java
@@ -2,31 +2,48 @@ package org.yt.jr.projects.maps;
 
 import org.yt.jr.projects.creatures.Creature;
 import org.yt.jr.projects.utils.Config;
-import org.yt.jr.projects.utils.CreaturesTypes;
+import org.yt.jr.projects.creatures.CreaturesTypes;
 import org.yt.jr.projects.utils.logs.LogLevels;
 import org.yt.jr.projects.utils.logs.LogSources;
 import org.yt.jr.projects.utils.logs.Logger;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.net.Inet4Address;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Location {
+    public static Location NOWHERE = new Location();
+    final private static AtomicInteger lastId = new AtomicInteger(1);
+    final private int id;
     final private int row;
     final private int col;
+
     // neighboring locations
-    final private Map<LocationNeighbors,Location> neighbors;
+    final private Map<LocationNeighbors, Location> neighbors;
     final private ArrayList<Creature> habitants;
 
-    final private Map<CreaturesTypes,Integer> creaturesCount;
+    final private Map<CreaturesTypes, Integer> creaturesCount;
+
+    private Location() {
+        this.id = 0;
+        this.row = Integer.MAX_VALUE;
+        this.col = Integer.MAX_VALUE;
+        habitants = new ArrayList<>();
+        neighbors = new HashMap<>();
+        creaturesCount = new HashMap<>();
+    }
 
     Location(int row, int col) {
+        this.id = lastId.addAndGet(1);
         this.row = row;
         this.col = col;
         habitants = new ArrayList<>();
         neighbors = new HashMap<>();
-
         creaturesCount = new HashMap<>();
+    }
+
+    public int getId() {
+        return id;
     }
 
     public ArrayList<Creature> getHabitants() {
@@ -40,24 +57,44 @@ public class Location {
     @Override
     public String toString() {
         return "Location{" +
-                row + "x" + col +
+                "id=" + id +
+                ", row=" + row +
+                ", col=" + col +
                 ", creaturesCount=" + creaturesCount +
                 '}';
     }
 
     void setNeighbor(LocationNeighbors direction, Location neighbor) {
-        this.neighbors.put(direction,neighbor);
+        this.neighbors.put(direction, neighbor);
     }
 
     public void addCreature(Creature creature) {
         CreaturesTypes type = creature.getType();
         habitants.add(creature);
         creature.setLocation(this);
-        creaturesCount.put(type,creaturesCount.getOrDefault(type,0)+1);
-        Logger.Log(LogSources.CREATURE, LogLevels.INFO,String.format("%s added to %s",creature, this));
+        creaturesCount.put(type, creaturesCount.getOrDefault(type, 0) + 1);
+        Logger.Log(LogSources.CREATURE, LogLevels.INFO, String.format("%s added to %s", creature, this));
     }
 
-    public boolean isSpaceAvailable(CreaturesTypes type) {
-        return creaturesCount.get(type) < Config.CONFIG.getMaxCreaturePerLocation(type);
+    public void removeCreature(Creature creature) {
+        CreaturesTypes type = creature.getType();
+        habitants.remove(creature);
+        creaturesCount.put(type, creaturesCount.get(type) - 1);
+        Logger.Log(LogSources.CREATURE, LogLevels.INFO, String.format("%s removed from %s", creature, this));
+        creature.setLocation(NOWHERE);
+    }
+
+    public boolean checkSpaceAvailable(CreaturesTypes type, boolean writeLogOnError) {
+        if (this == NOWHERE) {
+            return false;
+        }
+        if (creaturesCount.get(type) >= Config.CONFIG.getMaxCreaturePerLocation(type)) {
+            if (writeLogOnError) {
+                Logger.Log(LogSources.CREATURE, LogLevels.DEBUG,
+                        String.format("No slots for %s on %s", type, this));
+            }
+            return false;
+        }
+        return true;
     }
 }
