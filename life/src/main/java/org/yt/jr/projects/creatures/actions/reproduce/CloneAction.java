a93b32955d0f586702e0724847c06c84d106cd0d
Yury Trembach <yury.trembach@gmail.com>
2024-01-12 01:53:57 +0200

    Clone action - add new creature from one parent

---
+++ life/src/main/java/org/yt/jr/projects/creatures/actions/reproduce/CloneAction.java
@@ -0,0 +1,42 @@
+package org.yt.jr.projects.creatures.actions.reproduce;
+
+import org.yt.jr.projects.creatures.Creature;
+import org.yt.jr.projects.creatures.lifecycles.CreatureFactory;
+import org.yt.jr.projects.creatures.lifecycles.LifeCycle;
+import org.yt.jr.projects.maps.Location;
+import org.yt.jr.projects.creatures.CreaturesTypes;
+import org.yt.jr.projects.utils.Config;
+
+public class CloneAction {
+    final private Creature parent;
+    final private Location location;
+    final private CreaturesTypes childType;
+    final private LifeCycle lifeCycle;
+
+    public CloneAction(final Creature parent) {
+        this.parent = parent;
+        this.location = parent.getLocation();
+        this.childType = parent.getType();
+        this.lifeCycle = LifeCycle.LIFECYCLES.get(childType);
+    }
+
+    public void cloneChild() {
+        final Creature child;
+
+        synchronized (parent) {
+            synchronized (location) {
+                if (!parent.isReadyToReproduce(true)) {
+                    return;
+                }
+                parent.resetTurnsToReproduce();
+                child = CreatureFactory.CREATURE_FACTORY.getCreature(childType).apply(Config.CREATURE_CHILD_HEALTH);
+                synchronized (child) {
+                    location.addCreature(child);
+                    synchronized (lifeCycle) {
+                        lifeCycle.addCreature(child);
+                    }
+                }
+            }
+        }
+    }
+}
