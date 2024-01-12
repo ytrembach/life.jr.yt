26aeb390198602c8f7bc7e1100d41f51d71f0864
Yury Trembach <yury.trembach@gmail.com>
2024-01-12 02:07:45 +0200

    rewrite to use CloneAction

--- life/src/main/java/org/yt/jr/projects/creatures/Plant.java
+++ life/src/main/java/org/yt/jr/projects/creatures/Plant.java
@@ -1,10 +1,11 @@
 package org.yt.jr.projects.creatures;
 
+import org.yt.jr.projects.creatures.actions.reproduce.CloneAction;
 import org.yt.jr.projects.utils.Config;
-import org.yt.jr.projects.utils.CreaturesTypes;
 import org.yt.jr.projects.utils.logs.LogLevels;
 import org.yt.jr.projects.utils.logs.LogSources;
 import org.yt.jr.projects.utils.logs.Logger;
+
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Plant extends Creature {
@@ -13,16 +14,13 @@ public class Plant extends Creature {
     }
 
     @Override
-    public void reproduce() {
-        if (!location.isSpaceAvailable(type)) {
-            Logger.Log(LogSources.CREATURE,LogLevels.DEBUG,
-                    String.format("No slots for %s on %s",type,location));
-            return;
-        }
-        double probability = Config.CONFIG.getReproduceProbability(type);
-        double random = ThreadLocalRandom.current().nextDouble();
-        if (random < probability) {
-            bornChild();
+    public void tryToReproduce() {
+        if (isReadyToReproduce(false)) {
+            double probability = Config.CONFIG.getReproduceProbability(type);
+            if (ThreadLocalRandom.current().nextDouble() < probability) {
+                new CloneAction(this).cloneChild();
+                Logger.Log(LogSources.CREATURE, LogLevels.DEBUG, String.format("%s cloned", this));
+            }
         }
     }
 }
