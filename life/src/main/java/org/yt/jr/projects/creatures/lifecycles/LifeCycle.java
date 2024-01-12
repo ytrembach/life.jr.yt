b09bbe04d38eac696d64524c3f48f20d942bf659
Yury Trembach <yury.trembach@gmail.com>
2024-01-12 02:01:09 +0200

    add thread name showing its life cycle
    rewrite cancel to avoid race cond
    
    add ShowStatus

--- life/src/main/java/org/yt/jr/projects/creatures/lifecycles/LifeCycle.java
+++ life/src/main/java/org/yt/jr/projects/creatures/lifecycles/LifeCycle.java
@@ -2,22 +2,19 @@ package org.yt.jr.projects.creatures.lifecycles;
 
 import org.yt.jr.projects.creatures.Creature;
 import org.yt.jr.projects.utils.Config;
-import org.yt.jr.projects.utils.CreaturesTypes;
+import org.yt.jr.projects.creatures.CreaturesTypes;
 import org.yt.jr.projects.utils.logs.LogLevels;
 import org.yt.jr.projects.utils.logs.LogSources;
 import org.yt.jr.projects.utils.logs.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class LifeCycle implements AutoCloseable {
-
-    final public static Map<CreaturesTypes,LifeCycle> LIFECYCLES = new HashMap<>();
-
+    final private AtomicInteger count = new AtomicInteger(0);
+    final public static Map<CreaturesTypes, LifeCycle> LIFECYCLES = new HashMap<>();
     final private LifeCycleTypes type;
     final private Map<Creature, ScheduledFuture<?>> liveCreatures;
 
@@ -28,7 +25,8 @@ public class LifeCycle implements AutoCloseable {
         liveCreatures = new HashMap<>();
 
         int poolSize = Config.CONFIG.getPoolSize(type);
-        executorService = Executors.newScheduledThreadPool(poolSize);
+        executorService = Executors.newScheduledThreadPool(poolSize,
+                (r) -> new Thread(r, type.name().substring(0, 1) + "="));
     }
 
     public LifeCycleTypes getType() {
@@ -43,24 +41,42 @@ public class LifeCycle implements AutoCloseable {
     }
 
     public void addCreature(Creature creature) {
-        long turnDuration = Config.CONFIG.getTurnDurationInSecond(type);
-        ScheduledFuture<?> future = executorService.scheduleAtFixedRate(creature, turnDuration, turnDuration, TimeUnit.SECONDS);
+        long turnsPerMove = Config.CONFIG.getTurnsPerMove(type);
         synchronized (liveCreatures) {
+            ScheduledFuture<?> future = executorService.scheduleAtFixedRate(creature,
+                    turnsPerMove,
+                    ThreadLocalRandom.current().nextInt(900,1100) * turnsPerMove,
+                    TimeUnit.MILLISECONDS);
             liveCreatures.put(creature, future);
+            count.addAndGet(1);
         }
-        Logger.Log(LogSources.LIFECYCLE, LogLevels.INFO,String.format("%s added to %s",creature,this));
+        Logger.Log(LogSources.LIFECYCLE, LogLevels.INFO, String.format("%s added to %s", creature, this));
     }
 
     public void removeCreature(Creature creature) {
         ScheduledFuture<?> future = liveCreatures.get(creature);
-        future.cancel(false);
         synchronized (liveCreatures) {
+            if (!future.cancel(true) || !future.isDone()) {
+                Logger.Log(LogSources.LIFECYCLE,LogLevels.DEBUG,
+                        String.format("task %s wasn't successfully cancelled from %s",future,this));
+            }
             liveCreatures.remove(creature);
+            count.addAndGet(-1);
         }
     }
 
+    public void showStatus() {
+        System.out.printf("%s: count: %d ", this, count.get());
+        Map<CreaturesTypes, Integer> countByTypes = new HashMap<>();
+        for (Creature creature : liveCreatures.keySet()) {
+            CreaturesTypes type = creature.getType();
+            countByTypes.put(type, countByTypes.getOrDefault(type, 0) + 1);
+        }
+        System.out.println(countByTypes);
+    }
+
     @Override
-    public void close()  {
+    public void close() {
         executorService.shutdown();
     }
 }
