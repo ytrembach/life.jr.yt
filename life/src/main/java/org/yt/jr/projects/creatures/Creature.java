3d92345ddb4980d6740651e93cae50c91255c103
Yury Trembach <yury.trembach@gmail.com>
2024-01-13 15:40:54 +0200

    cosmetic

--- life/src/main/java/org/yt/jr/projects/creatures/Creature.java
+++ life/src/main/java/org/yt/jr/projects/creatures/Creature.java
@@ -7,7 +7,6 @@ import org.yt.jr.projects.utils.logs.LogLevels;
 import org.yt.jr.projects.utils.logs.LogSources;
 import org.yt.jr.projects.utils.logs.Logger;
 
-import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
 
