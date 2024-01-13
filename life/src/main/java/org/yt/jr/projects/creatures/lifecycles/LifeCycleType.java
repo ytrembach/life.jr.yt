04e73bcb074533364fb83a8d9d92cf6b7e311160
Yury Trembach <yury.trembach@gmail.com>
2024-01-13 12:39:18 +0200

    rename type

---
+++ life/src/main/java/org/yt/jr/projects/creatures/lifecycles/LifeCycleType.java
@@ -0,0 +1,24 @@
+package org.yt.jr.projects.creatures.lifecycles;
+
+import org.yt.jr.projects.creatures.CreatureType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum LifeCycleType {
+    EMPTY,
+    PLANTS,
+    HERBIVORES,
+    CARNIVORES;
+
+    public List<CreatureType> getTypes() {
+        final List<CreatureType> typesList = new ArrayList<>();
+
+        for (CreatureType creatureType : CreatureType.values()) {
+            if (this.equals(creatureType.getLifeCycleType())) {
+                typesList.add(creatureType);
+            }
+        }
+        return typesList;
+    }
+}
