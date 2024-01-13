ed83617debf5935fa970ccabe572069bf0aa680f
Yury Trembach <yury.trembach@gmail.com>
2024-01-13 12:49:25 +0200

    rename types apply new Config methods names set initial random

--- life/src/main/java/org/yt/jr/projects/creatures/lifecycles/LifeCycleCreator.java
+++ life/src/main/java/org/yt/jr/projects/creatures/lifecycles/LifeCycleCreator.java
@@ -4,29 +4,33 @@ import org.yt.jr.projects.creatures.Creature;
 import org.yt.jr.projects.maps.Location;
 import org.yt.jr.projects.maps.WorldMap;
 import org.yt.jr.projects.utils.Config;
-import org.yt.jr.projects.creatures.CreaturesTypes;
+import org.yt.jr.projects.creatures.CreatureType;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class LifeCycleCreator {
-    public static LifeCycle createLifeCycle(final LifeCycleTypes type) {
+    public static LifeCycle createLifeCycle(final LifeCycleType type) {
         final LifeCycle lifeCycle = new LifeCycle(type);
 
         for (Location location : WorldMap.getMap()) {
-            for (CreaturesTypes creaturesType : lifeCycle.getType().getTypes()) {
-                initLocation(lifeCycle, location, creaturesType);
-                LifeCycle.LIFECYCLES.put(creaturesType, lifeCycle);
+            for (CreatureType creatureType : lifeCycle.getType().getTypes()) {
+                initLocation(lifeCycle, location, creatureType);
+                LifeCycle.LIFECYCLES.put(creatureType, lifeCycle);
             }
         }
         return lifeCycle;
     }
 
-    private static void initLocation(final LifeCycle lifeCycle, final Location location, final CreaturesTypes type) {
-        final int maxCreatures = Config.CONFIG.getMaxCreaturePerLocation(type);
-        final double initShare = Config.CONFIG.getInitCreaturePerLocationShare(type);
+    private static void initLocation(final LifeCycle lifeCycle, final Location location, final CreatureType type) {
+        final int maxCreatures = Config.CONFIG.maxCreaturePerLocation(type);
+        final double initShare = Config.CONFIG.initCreaturePerLocationShare(type);
 
-        final long initCreatureCount = Math.round(maxCreatures * initShare);
+        final long initCreatureCount =
+                Math.round(maxCreatures * initShare * ThreadLocalRandom.current().nextDouble());
 
         for (int c = 0; c < initCreatureCount; c++) {
-            final Creature creature = CreatureFactory.CREATURE_FACTORY.getCreature(type).apply(Config.CREATURE_MAX_HEALTH);
+            final Creature creature = CreatureFactory.CREATURE_FACTORY.getCreature(type)
+                    .apply(ThreadLocalRandom.current().nextInt(1,Config.CONFIG.creatureDefaultHealth("max")));
             synchronized (creature) {
                 synchronized (location) {
                     location.addCreature(creature);
