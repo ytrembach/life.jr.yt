fbdf9e92eae0f9da4bd90953dfdfa331bea31a31
Yury Trembach <yury.trembach@gmail.com>
2024-01-13 12:46:14 +0200

    rename types apply new Config methods names

--- life/src/main/java/org/yt/jr/projects/creatures/animals/Deer.java
+++ life/src/main/java/org/yt/jr/projects/creatures/animals/Deer.java
@@ -1,12 +1,12 @@
 package org.yt.jr.projects.creatures.animals;
 
 import org.yt.jr.projects.creatures.Animal;
-import org.yt.jr.projects.creatures.CreaturesTypes;
+import org.yt.jr.projects.creatures.CreatureType;
 
 public class Deer extends Animal {
 
     public Deer(int health) {
-        super(CreaturesTypes.DEER, health);
+        super(CreatureType.DEER, health);
     }
 
 }
