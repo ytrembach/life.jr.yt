777258ca02e0f69b62317997d1ceafdb04fff562
Yury Trembach <yury.trembach@gmail.com>
2024-01-13 22:04:34 +0200

    rewrite to Check/Action

--- life/src/main/java/org/yt/jr/projects/creatures/Animal.java
+++ life/src/main/java/org/yt/jr/projects/creatures/Animal.java
@@ -1,17 +1,23 @@
 package org.yt.jr.projects.creatures;
 
 import org.yt.jr.projects.creatures.actions.reproduce.BornAction;
+import org.yt.jr.projects.creatures.actions.reproduce.BornCheck;
+
+import java.util.Optional;
 
 public abstract class Animal extends Creature {
+
     public Animal(CreatureType type, int health) {
         super(type, health);
+        reproduceCheck = BornCheck::new;
+        reproduceAction = BornAction::new;
     }
 
     @Override
-    public void tryToReproduce() {
-        final BornAction bornAction = new BornAction(this);
-        if (bornAction.checkReady()) {
-            bornAction.doAction();
+    protected void tryToReproduce() {
+        Optional<Creature> partyFound =reproduceCheck.apply(this).checkPartyToAct();
+        if (partyFound.isPresent()) {
+            reproduceAction.apply(this).doAction(partyFound.get());
         }
     }
 }
--- life/src/main/java/org/yt/jr/projects/creatures/Plant.java
+++ life/src/main/java/org/yt/jr/projects/creatures/Plant.java
@@ -1,17 +1,20 @@
 package org.yt.jr.projects.creatures;
 
 import org.yt.jr.projects.creatures.actions.reproduce.CloneAction;
+import org.yt.jr.projects.creatures.actions.reproduce.CloneCheck;
 
 public class Plant extends Creature {
+
     public Plant(int health) {
         super(CreatureType.PLANT, health);
+        reproduceCheck = CloneCheck::new;
+        reproduceAction = CloneAction::new;
     }
 
     @Override
-    public void tryToReproduce() {
-        final CloneAction cloneAction = new CloneAction(this);
-        if (cloneAction.checkReady()) {
-            cloneAction.doAction();
+    protected void tryToReproduce() {
+        if (reproduceCheck.apply(this).checkPartyToAct().isPresent()) {
+            reproduceAction.apply(this).doAction(this);
         }
     }
 }
