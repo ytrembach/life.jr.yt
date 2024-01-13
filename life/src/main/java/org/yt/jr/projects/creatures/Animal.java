b78a9639d75d5947fb234f4a27a5803cbc502a44
Yury Trembach <yury.trembach@gmail.com>
2024-01-13 17:29:55 +0200

    move to Action interface

--- life/src/main/java/org/yt/jr/projects/creatures/Animal.java
+++ life/src/main/java/org/yt/jr/projects/creatures/Animal.java
@@ -1,14 +1,6 @@
 package org.yt.jr.projects.creatures;
 
 import org.yt.jr.projects.creatures.actions.reproduce.BornAction;
-import org.yt.jr.projects.utils.Config;
-import org.yt.jr.projects.utils.logs.LogLevels;
-import org.yt.jr.projects.utils.logs.LogSources;
-import org.yt.jr.projects.utils.logs.Logger;
-
-
-import java.util.Optional;
-import java.util.concurrent.ThreadLocalRandom;
 
 public abstract class Animal extends Creature {
     public Animal(CreatureType type, int health) {
@@ -17,35 +9,9 @@ public abstract class Animal extends Creature {
 
     @Override
     public void tryToReproduce() {
-        if (isReadyToReproduce(false)) {
-            Logger.Log(LogSources.CREATURE, LogLevels.DEBUG, String.format("trying to reproduce %s in %s", this, location));
-            double probability = Config.CONFIG.reproduceProbability(type);
-            if (ThreadLocalRandom.current().nextDouble() < probability) {
-                Optional<Animal> found = findPairToReproduce();
-                if (found.isPresent()) {
-                    Animal pair = found.get();
-                    if (new BornAction(this, pair).doAction()) {
-                        Logger.Log(LogSources.CREATURE, LogLevels.INFO,
-                                String.format("%s successfully paired with %s", this, pair));
-                    } else {
-                        Logger.Log(LogSources.CREATURE, LogLevels.ERROR,
-                                String.format("%s failed to pair with %s", this, pair));
-                    }
-                } else {
-                    Logger.Log(LogSources.CREATURE, LogLevels.DEBUG, String.format("pair not found for %s", this));
-                }
-            } else {
-                Logger.Log(LogSources.CREATURE, LogLevels.DEBUG, String.format("probability failed for %s", this));
-            }
+        final BornAction bornAction = new BornAction(this);
+        if (bornAction.checkReady()) {
+            bornAction.doAction();
         }
-    }
-
-    private Optional<Animal> findPairToReproduce() {
-        return location.getHabitants().stream()
-                .filter(creature -> creature.getType().equals(type))
-                .filter(creature -> creature.isReadyToReproduce(false))
-                .map(creature -> (Animal) creature)
-                .filter(creature -> this != creature)
-                .findAny();
     }
 }
--- life/src/main/java/org/yt/jr/projects/creatures/Plant.java
+++ life/src/main/java/org/yt/jr/projects/creatures/Plant.java
@@ -1,12 +1,6 @@
 package org.yt.jr.projects.creatures;
 
 import org.yt.jr.projects.creatures.actions.reproduce.CloneAction;
-import org.yt.jr.projects.utils.Config;
-import org.yt.jr.projects.utils.logs.LogLevels;
-import org.yt.jr.projects.utils.logs.LogSources;
-import org.yt.jr.projects.utils.logs.Logger;
-
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Plant extends Creature {
     public Plant(int health) {
@@ -15,15 +9,9 @@ public class Plant extends Creature {
 
     @Override
     public void tryToReproduce() {
-        if (isReadyToReproduce(false)) {
-            double probability = Config.CONFIG.reproduceProbability(type);
-            if (ThreadLocalRandom.current().nextDouble() < probability) {
-                if (new CloneAction(this).doAction()) {
-                    Logger.Log(LogSources.CREATURE, LogLevels.INFO, String.format("%s successfully cloned", this));
-                } else {
-                    Logger.Log(LogSources.CREATURE, LogLevels.ERROR, String.format("%s failed to clone in cloneAction", this));
-                }
-            }
+        final CloneAction cloneAction = new CloneAction(this);
+        if (cloneAction.checkReady()) {
+            cloneAction.doAction();
         }
     }
 }
