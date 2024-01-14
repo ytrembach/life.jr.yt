919f4451bd75ed1a9de8296f940928b962576052
Yury Trembach <yury.trembach@gmail.com>
2024-01-14 02:25:55 +0200

    remove warnings

--- life/src/main/java/org/yt/jr/projects/creatures/actions/eat/FoodMatrix.java
+++ life/src/main/java/org/yt/jr/projects/creatures/actions/eat/FoodMatrix.java
@@ -1,6 +1,5 @@
 package org.yt.jr.projects.creatures.actions.eat;
 
-import org.yt.jr.projects.creatures.Creature;
 import org.yt.jr.projects.creatures.CreatureType;
 import org.yt.jr.projects.utils.Config;
 
@@ -11,7 +10,7 @@ public class FoodMatrix {
 
     final public static FoodMatrix FOOD_MATRIX = new FoodMatrix();
 
-    private Map<CreatureType, Map<CreatureType, Double>> foodMatrix = new HashMap();
+    final private HashMap<CreatureType, HashMap<CreatureType, Double>> foodMatrix = new HashMap();
 
     public void initFoodMatrix() {
         for (CreatureType eaterType : CreatureType.values()) {
@@ -38,7 +37,7 @@ public class FoodMatrix {
     }
 
     public void setProbability(final CreatureType eaterType, final CreatureType foodType, final Double probability) {
-        Map<CreatureType, Double> eaterMap = foodMatrix.get(eaterType);
+        HashMap<CreatureType, Double> eaterMap = foodMatrix.get(eaterType);
         if (eaterMap == null) {
             eaterMap = new HashMap<>();
             foodMatrix.put(eaterType, eaterMap);
