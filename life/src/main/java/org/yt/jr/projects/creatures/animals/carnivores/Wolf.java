dc641e888997f02302e49036ec61378ce8aec288
Yury Trembach <yury.trembach@gmail.com>
2024-01-18 01:22:47 +0200

    moved to packages

--- life/src/main/java/org/yt/jr/projects/creatures/animals/carnivores/Wolf.java
+++ life/src/main/java/org/yt/jr/projects/creatures/animals/carnivores/Wolf.java
@@ -1,4 +1,4 @@
-package org.yt.jr.projects.creatures.animals;
+package org.yt.jr.projects.creatures.animals.carnivores;
 
 import org.yt.jr.projects.creatures.Animal;
 import org.yt.jr.projects.creatures.CreatureType;
--- life/src/main/java/org/yt/jr/projects/creatures/animals/herbivores/Deer.java
+++ life/src/main/java/org/yt/jr/projects/creatures/animals/herbivores/Deer.java
@@ -1,4 +1,4 @@
-package org.yt.jr.projects.creatures.animals;
+package org.yt.jr.projects.creatures.animals.herbivores;
 
 import org.yt.jr.projects.creatures.Animal;
 import org.yt.jr.projects.creatures.CreatureType;
