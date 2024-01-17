e05753e0b88a9c14edb7e2a40f80abc94d385b7b
Yury Trembach <yury.trembach@gmail.com>
2024-01-18 01:24:41 +0200

    added new CreatureTypes to new animals added to Factory

--- life/src/main/java/org/yt/jr/projects/creatures/CreatureType.java
+++ life/src/main/java/org/yt/jr/projects/creatures/CreatureType.java
@@ -3,14 +3,24 @@ package org.yt.jr.projects.creatures;
 import org.yt.jr.projects.creatures.lifecycles.LifeCycleType;
 
 public enum CreatureType {
-    NONEXISTENT(LifeCycleType.EMPTY),
     PLANT(LifeCycleType.PLANTS),
 
     HORSE(LifeCycleType.HERBIVORES),
     DEER(LifeCycleType.HERBIVORES),
+    RABBIT(LifeCycleType.HERBIVORES),
+    MOUSE(LifeCycleType.HERBIVORES),
+    GOAT(LifeCycleType.HERBIVORES),
+    SHEEP(LifeCycleType.HERBIVORES),
+    BOAR(LifeCycleType.HERBIVORES),
+    BUFFALO(LifeCycleType.HERBIVORES),
+    DUCK(LifeCycleType.HERBIVORES),
+    CATERPILLAR(LifeCycleType.HERBIVORES),
 
-    WOLF(LifeCycleType.CARNIVORES)
-    ;
+    WOLF(LifeCycleType.CARNIVORES),
+    BOA(LifeCycleType.CARNIVORES),
+    FOX(LifeCycleType.CARNIVORES),
+    BEAR(LifeCycleType.CARNIVORES),
+    EAGLE(LifeCycleType.CARNIVORES);
 
     final private LifeCycleType lifeCycleType;
 
--- life/src/main/java/org/yt/jr/projects/creatures/lifecycles/CreatureFactory.java
+++ life/src/main/java/org/yt/jr/projects/creatures/lifecycles/CreatureFactory.java
@@ -2,24 +2,37 @@ package org.yt.jr.projects.creatures.lifecycles;
 
 import org.yt.jr.projects.creatures.Creature;
 import org.yt.jr.projects.creatures.Plant;
-import org.yt.jr.projects.creatures.animals.Deer;
-import org.yt.jr.projects.creatures.animals.Horse;
+import org.yt.jr.projects.creatures.animals.carnivores.*;
+import org.yt.jr.projects.creatures.animals.herbivores.*;
 import org.yt.jr.projects.creatures.CreatureType;
-import org.yt.jr.projects.creatures.animals.Wolf;
 
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
+
 
 public class CreatureFactory {
     final public static CreatureFactory CREATURE_FACTORY = new CreatureFactory();
 
-    final private Map<CreatureType, Function<Integer, Creature>> generators = Map.of(
-            CreatureType.PLANT, Plant::new,
+    final private Map<CreatureType, Function<Integer, Creature>> generators = Map.ofEntries(
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.PLANT, Plant::new),
 
-            CreatureType.HORSE, Horse::new,
-            CreatureType.DEER, Deer::new,
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.HORSE, Horse::new),
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.DEER, Deer::new),
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.RABBIT, Rabbit::new),
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.MOUSE, Mouse::new),
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.GOAT, Goat::new),
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.SHEEP, Sheep::new),
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.BOAR, Boar::new),
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.BUFFALO, Buffalo::new),
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.DUCK, Duck::new),
 
-            CreatureType.WOLF, Wolf::new
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.CATERPILLAR, Caterpillar::new),
+
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.WOLF, Wolf::new),
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.BOA, Boa::new),
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.FOX, Fox::new),
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.BEAR, Bear::new),
+            new AbstractMap.SimpleImmutableEntry<CreatureType, Function<Integer, Creature>> (CreatureType.EAGLE, Eagle::new)
     );
 
     public Function<Integer, Creature> getCreature(CreatureType type) {
